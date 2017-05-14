4d2e1dcb60784ea6ffcd7928e8d3e9fe9d8004a7
Matt Johnson <mrjoh3@gmail.com>
2017-05-14 21:56:38 +1000

    added summary plot that simulates entries being added

--- app.js
+++ app.js
@@ -63,5 +63,84 @@ var health_data = [{"date": new Date("2011-08-28"),"value": 0.48},
                 height: 200,
                 right: 140,
                 yax_count: 3,
-                target: '#health-crt'
+                target: '#health-crt',
+                legend: ['Health','Roads','Community',
+                         'Education','Public Transport','Gender Equality',
+                         'Regions','Family Violence']
             });
+
+
+// LIVE UPDATE
+
+/**
+ * Randomize array element order in-place.
+ * Using Durstenfeld shuffle algorithm.
+ */
+function shuffleArray(array) {
+    for (var i = array.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+    return array;
+}
+
+
+
+var lv = [[{"n": 0,"value": 1.0}],
+               [{"n": 0,"value": 0.85}],
+               [{"n": 0,"value": 0.7}],
+               [{"n": 0,"value": 0.55}],
+               [{"n": 0,"value": 0.4}],
+               [{"n": 0,"value": 0.25}],
+               [{"n": 0,"value": 0.1}],
+               [{"n": 0,"value": 0.0}]
+              ];
+
+var pal = palette('tol-rainbow', 8).map(function(p){return '#' + p});
+
+MG.data_graphic({
+  data: lv,
+  height: 300,
+  width: 1000,
+  right: 140,
+  colors: pal,
+  target: '#live-plot',
+  x_accessor: 'n',
+  y_accessor: 'value'
+})
+
+function updatePlot(lv, pal, step) {
+    // lv is the data, p priority, n the iteration
+    var start = lv[0].length
+    var vals = [1.0, 0.85, 0.7, 0.55, 0.4, 0.25, 0.1, 0.0];
+    var tmp;
+
+
+    for (n = start; n < start + step; n++) {
+
+        tmp = shuffleArray(vals)
+
+        for (p = 0; p < 8; p++) {
+            var nv = tmp[p] + lv[p][n-1].value;
+            lv[p].push({'n': n, 'value': nv})
+        }
+
+        MG.data_graphic({
+          data: lv,
+          height: 300,
+          width: 1000,
+          right: 140,
+          colors: pal,
+          target: '#live-plot',
+          x_accessor: 'n',
+          y_accessor: 'value',
+          legend: ['Health','Roads','Community',
+                   'Education','Public Transport','Gender Equality',
+                   'Regions','Family Violence']
+        })
+    }
+}
+
+updatePlot(lv = lv, pal = pal, step = 10)
